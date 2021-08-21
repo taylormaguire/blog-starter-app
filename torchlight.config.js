0104b7b3821c4a5bb2574ad74f81d84df0e6fb47
Taylor Maguire <taylor@codingfoundry.com>
2021-08-20 22:36:13 -0700

    torchlight update

--- torchlight.config.js
+++ torchlight.config.js
@@ -11,7 +11,7 @@ module.exports = {
 
     // Which theme you want to use. You can find all of the themes at
     // https://torchlight.dev/docs/themes.
-    theme: 'material-theme-default',
+    theme: 'material-theme-palenight',
 
     // The Host of the API.
     host: 'https://api.torchlight.dev',
